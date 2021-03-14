09d5b66f3a9cb737806b0d493b4b4fb8d05b72af
Faraz <sayyedfaraz11@gmail.com>
2021-03-13 23:12:59 -0500

    initial commit

--- server/routes/demo.ts
+++ server/routes/demo.ts
@@ -1,4 +1,4 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
 
 const router = Router();
 
@@ -7,5 +7,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+router.get('/developer', function(req:Request, res:Response, next) {
+  res.json({"developers":["faraz"]}).status(200)
+});
 
 export default router;
--- server/routes/demoRoute.test.ts
+++ server/routes/demoRoute.test.ts
@@ -1,6 +1,8 @@
+import supertest from 'supertest';
 import request from 'supertest';
 import { setTestDb } from '../shared/functions';
 import app from '../app';
+
 
 // Isn't needed for this test but required for all tests that interact with the database.
 // The name of the database must be unique.
@@ -12,6 +14,14 @@ describe('A sample test demonstrating how to test APIs', () => {
 
         expect(result.status).toBe(200);
         expect(result.text.startsWith("<!DOCTYPE html>")).toBeTruthy();
+        done();
+    });
+
+    it('Request /developer should return HTTP 200 OK and length should be 1', async (done) => {
+        const result = await request(app).get('/developer').send();
+
+        expect(result.status).toBe(200);
+        expect(result.body.developers).toHaveLength(1)
         done();
     });
 });
