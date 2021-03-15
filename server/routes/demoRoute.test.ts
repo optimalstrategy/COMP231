6eb4fd56814b867dc9ed671152ee8e8703039c3f
Dmitriy Morozov <dmitry.mrzv@gmail.com>
2021-03-15 10:06:08 -0400

    Updated test

--- server/routes/demoRoute.test.ts
+++ server/routes/demoRoute.test.ts
@@ -1,25 +1,25 @@
-import request from 'supertest';
-import { setTestDb } from '../shared/functions';
-import app from '../app';
+import request from "supertest";
+import { setTestDb } from "../shared/functions";
+import app from "../app";
 
 // Isn't needed for this test but required for all tests that interact with the database.
 // The name of the database must be unique.
 setTestDb("demo-test-db");
 
-describe('A sample test demonstrating how to test APIs', () => {
-    it('Request / should return HTTP 200 OK', async (done) => {
-        const result = await request(app).get('/').send();
+describe("A sample test demonstrating how to test APIs", () => {
+  it("Request / should return HTTP 200 OK", async (done) => {
+    const result = await request(app).get("/").send();
 
-        expect(result.status).toBe(200);
-        expect(result.text.startsWith("<!DOCTYPE html>")).toBeTruthy();
-        done();
-    });
-    it('Request /developers should return developer names', async (done) => {
-        const result = await request(app).get('/developers').send();
+    expect(result.status).toBe(200);
+    expect(result.text.startsWith("<!DOCTYPE html>")).toBeTruthy();
+    done();
+  });
+  it("Request /developers should return developer names", async (done) => {
+    const result = await request(app).get("/developers").send();
 
-        expect(result.status).toBe(200);
-        const developers: String[] = result.body.developers;
-        expect(developers).toEqual(["George", "Hung"]);
-        done();
-    })
+    expect(result.status).toBe(200);
+    const developers: String[] = result.body.developers;
+    expect(developers).toEqual(["George", "Hung", "Dmitriy"]);
+    done();
+  });
 });
