5d320bdb66790b4136abd8c2f07365ab6a3c6bbb
Faraz <sayyedfaraz11@gmail.com>
2021-04-09 22:13:54 -0400

    adding login page

--- server/public/js/main.js
+++ server/public/js/main.js
@@ -1,7 +1,16 @@
+<<<<<<< HEAD
 // click yo scroll top 
 
 $('.move-up span').click(function (){
     $('html,body').animate({
         scrollTop: 0
     }, 1000);
+=======
+// click yo scroll top 
+
+$('.move-up span').click(function (){
+    $('html,body').animate({
+        scrollTop: 0
+    }, 1000);
+>>>>>>> ce70d4c... adding login page
 });
--- server/server.ts
+++ server/server.ts
@@ -2,7 +2,11 @@ import app from './app';
 import Logger from './shared/logger';
 
 // Start the server
+<<<<<<< HEAD
 const port = Number(process.env.PORT || 3000);
+=======
+const port = Number(process.env.PORT || 8080);
+>>>>>>> ce70d4c... adding login page
 app.listen(port, () => {
     Logger.info('Express server started on port: http://localhost:' + port);
 });
